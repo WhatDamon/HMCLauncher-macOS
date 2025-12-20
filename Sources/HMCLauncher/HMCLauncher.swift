4a372fcf3bf1b7cbcccfe595af338758247fc9ee
Damon  Lu <59256766+WhatDamon@users.noreply.github.com>
2025-12-20 12:28:35 +0800

    Catch error throws

--- Sources/HMCLauncher/HMCLauncher.swift
+++ Sources/HMCLauncher/HMCLauncher.swift
@@ -17,8 +17,45 @@ struct HMCLauncher {
                 print("Auto select JAVA_HOME: \(path)")
                 javaHome = path
             }
+        } catch JavaSelectionError.invalidJavaHome(_) {
+            showDialog(
+                L.t("HMCL_JAVA_HOME_INVALID"),
+                title: L.t("WARNING_TITLE"),
+                isWarning: true
+            )
+            exit(1)
+        } catch JavaSelectionError.newestTooLow(let JavaInstallation, _) {
+            showDialog(
+                L.t(
+                    "JAVA_TOO_OLD", "\(hmclExpectedJavaMajorVersion)",
+                    "\(JavaInstallation.versionStr)"),
+                title: L.t("JAVA_NOT_SUPPORTED_TITLE"),
+                buttons: [L.t("DOWNLOAD_JAVA_BUTTON"), L.t("CANCEL_BUTTON")],
+                isWarning: true
+            ) { button in
+                if button == L.t("DOWNLOAD_JAVA_BUTTON") {
+                    downloadJava()
+                }
+            }
+            exit(1)
+        } catch JavaSelectionError.userSpecifiedJavaVersionTooLow(
+            _, let detectedVersion, _)
+        {
+            showDialog(
+                L.t(
+                    "JAVA_TOO_OLD", "\(hmclExpectedJavaMajorVersion)", "\(detectedVersion)"),
+                title: L.t("JAVA_NOT_SUPPORTED_TITLE"),
+                buttons: [L.t("DOWNLOAD_JAVA_BUTTON"), L.t("CANCEL_BUTTON")],
+                isWarning: true
+            ) { button in
+                if button == L.t("DOWNLOAD_JAVA_BUTTON") {
+                    downloadJava()
+                }
+            }
+            exit(1)
         } catch let e as JavaSelectionError {
             print(e.description)
+            showDialog(L.t("ERROR_OCCURRED", "\(e.description)"), isWarning: true)
             exit(1)
         } catch {
             print("Unknown error: \(error)")
