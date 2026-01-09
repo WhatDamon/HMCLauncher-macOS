0f9d443af9eb1accab4532d015322341ea045fee
Damon  Lu <59256766+WhatDamon@users.noreply.github.com>
2026-01-09 19:35:21 +0800

    Logging fix

--- Sources/HMCLauncher/Debug.swift
+++ Sources/HMCLauncher/Debug.swift
@@ -1,14 +1,7 @@
 import Foundation
 
 // MARK: - Debug Logger
-
 struct DebugLogger {
-
-    // MARK: Configuration
-    static var isEnabled: Bool { LauncherEnv.isDebug }
-    static let logURL: URL? = nil  // set externally if needed
-
-    // MARK: Log Level
     enum Level: String {
         case debug = "DEBUG"
         case info = "INFO"
@@ -16,11 +9,8 @@ struct DebugLogger {
         case error = "ERROR"
     }
 
-    // MARK: Private State
-    private static let queue = DispatchQueue(
-        label: "hmclauncher.debug.logger",
-        qos: .utility
-    )
+    static var isEnabled: Bool { LauncherEnv.isDebug }
+    static let logURL: URL? = nil
 
     private static let formatter: DateFormatter = {
         let f = DateFormatter()
@@ -28,7 +18,6 @@ struct DebugLogger {
         return f
     }()
 
-    // MARK: Logging API
     static func log(
         _ message: String,
         level: Level = .debug,
@@ -37,25 +26,20 @@ struct DebugLogger {
     ) {
         guard isEnabled else { return }
 
-        queue.async {
-            let time = formatter.string(from: Date())
-            let filename = file.split(separator: "/").last ?? ""
+        let time = formatter.string(from: Date())
+        let filename = file.split(separator: "/").last ?? ""
+        let lineText =
+            "[\(time)][\(level.rawValue)][HMCLauncher-macOS][\(filename):\(line)] \(message)"
 
-            let lineText =
-                "[\(time)][\(level.rawValue)][HMCLauncher-macOS][\(filename):\(line)] \(message)"
+        print(lineText)
 
-            print(lineText)
-
-            if let url = logURL, let data = (lineText + "\n").data(using: .utf8) {
-                append(data, to: url)
-            }
+        if let url = logURL, let data = (lineText + "\n").data(using: .utf8) {
+            append(data, to: url)
         }
     }
 
-    // MARK: File Writing
     private static func append(_ data: Data, to url: URL) {
         let fm = FileManager.default
-
         if fm.fileExists(atPath: url.path),
             let handle = try? FileHandle(forWritingTo: url)
         {
